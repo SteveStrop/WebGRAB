4787047efa6af9c6254fa8695a946b89b2d744ab
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-01 16:08:50 +0100

    empty javascript project folder webpack and babel ready

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,3 @@
 window.alert('index.js initialised and linked to style sheet if background is blue')
+
+console.log(`${'test string for babel'}`)
--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ const path = require('path') // use built in node absolute path variable
 const HtmlWebpackPlugin = require('html-webpack-plugin') // creates distribution files from source templates
 module.exports = {
 
-  // file(s) webpack uses to bundle everything from (will contain multiple import lines)
-  entry: './src/js/index.js',
+  // file(s) webpack uses to bundle everything from (will contain multiple import lines). poly fills go here too
+  entry: ['babel-polyfill', './src/js/index.js'],
   // path to and filename of file webpack uses to bundle everything into
   output: {
     path: path.resolve(__dirname, 'dist'), // __dirname is absolute path of current working directory. usage: (_dirname,relative path of distribution folder)
@@ -17,6 +17,17 @@ module.exports = {
       filename: 'index.html', // name of file in output path to create i.e distribution file to create. CHANGE THIS IF YOU DON'T WANT index.html
       template: './src/index.html'// name of template file to base above filename on. CHANGE THIS IF YOU DON'T WANT index.html
     })
-  ]
-
+  ],
+  // setup babel
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // regexp to select files that end in .js anywhere within the root folder structure
+        exclude: /node_modules/, // regexp to exclude node_modules folder
+        use: {
+          loader: 'babel-loader' // use babel loader when compiling .js files
+        }
+      }
+    ]
+  }
 }
