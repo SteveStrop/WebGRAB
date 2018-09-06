102f6a5f9ee2489fcd10eba7109f35736b517357
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-06 08:56:02 +0100

    improved status message logic

--- src/js/models/Status.js
+++ src/js/models/Status.js
@@ -1,23 +1,49 @@
 import { DOM } from '../config'
 export default class Status {
-  renderProgress (n, d = 1) {
-    const segment = n * 100 / d
+  renderProgress (index, total = 1) {
+    const segment = index * 100 / total // set fractional width
     DOM.bar.style.width = `${segment}%`
   }
   showProgressBar (show) {
+    // display progress bar div if show is true
     DOM.progressBar.setAttribute('class', show ? '' : 'hidden')
   }
-  updateStatus (state) {
+  updateStatusMessage (state) {
     DOM.statusMessage.innerText = getStatus(state)
   }
 }
-const getStatus = state => {
+const getStatus2 = state => {
+  // create message strings
   const messages = {
     notRenderComplete: 'Select photos',
     notDestination: 'Choose destination folder',
     importEnabled: 'Import (renamed) photos'
   }
+  // create selection criteria
   if (!DOM.importBtn.disabled) return messages.importEnabled
   if (!state.photos.renderComplete) return messages.notRenderComplete
   if (!state.destinations.currentFolder) return messages.notDestination
+}
+
+const getStatus = state => {
+  /*
+  |* returns message associated with FIRST true criteria
+  */
+  // create message strings
+  const messages = new Map([
+    [1, 'Select photos'],
+    [2, 'Choose destination folder'],
+    [3, 'Import (renamed) photos']
+  ])
+  // create selection criteria MUST map 1 for 1 with message string above
+  const criteria = new Map([
+    [1, !DOM.importBtn.disabled],
+    [2, !state.photos.renderComplete],
+    [3, !state.destinations.currentFolder]
+  ])
+  for (let i = 0; i < criteria.size; i++) {
+    if (criteria.get(i)) { // if true
+      return messages.get(i) // return corresponding message
+    }
+  }
 }
