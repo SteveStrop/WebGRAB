3baafa01e19f21fc577b86c92e93ced523ab4f85
SteveStrop <steve_tropical@yahoo.co.uk>
2018-09-06 12:34:54 +0100

    used destructuring

--- src/js/models/Status.js
+++ src/js/models/Status.js
@@ -9,36 +9,17 @@ export default class Status {
     DOM.progressBar.setAttribute('class', show ? '' : 'hidden')
   }
   updateStatusMessage (state) {
-    DOM.statusMessage.innerText = getStatus(state)
-  }
-}
-const getStatus = state => {
-  /*
-  |* returns message associated with FIRST true condition
-  */
-  // create message conditions and strings [index,[condition,message]]
-  const messages = new Map([
-    [0,
-      [DOM.importBtn.disabled && !state.photos.renderComplete, 'Select photos']
-    ],
-    [1,
-      [state.photos.renderComplete && !state.destinations.currentFolder, 'Choose destination folder']
-    ],
-    [2,
+    /*
+   |* returns message associated with FIRST true condition
+   */
+    // create message conditions and strings [condition,message]
+    const messages = [
+      [DOM.importBtn.disabled && !state.photos.renderComplete, 'Select photos'],
+      [state.photos.renderComplete && !state.destinations.currentFolder, 'Choose destination folder'],
       [state.destinations.currentFolder, 'Ready to import (renamed) photos']
     ]
-  ])
-  window.m = messages
-  // create selection criteria MUST map 1 for 1 with message string above
-  /* const criteria = new Map([
-    [0, DOM.importBtn.disabled && !state.photos.renderComplete],
-    [1, state.photos.renderComplete && !state.destinations.currentFolder],
-    [2, state.destinations.currentFolder]
-  ])
-  */
-  for (const value of messages.values()) {
-    const condition = value[0]
-    const message = value[1]
-    if (condition) return message // return corresponding message
+    for (const [condition, message] of messages) {
+      if (condition) DOM.statusMessage.innerText = message // return corresponding message
+    }
   }
 }
